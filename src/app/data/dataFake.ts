4ac5e3977683f2aafc7a95b06694476804b20759
Kelwin Cavalheiro <kelwinkiwi@gmail.com>
2024-02-22 18:46:19 -0300

    alterando valor da data fake

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,26 +2,26 @@ export const dataFake = [
     {
         "id":"1",
         "photo": "https://staticg.sportskeeda.com/editor/2023/12/2a78a-17033006435683-1920.jpg?w=840",
-        "title": "Olha o choso ai ",
-        "description": "Choso é um personagem legal"
+        "title": "Choso",
+        "description": "Choso (脹ちょう相そう Choso ? )é um personagem coadjuvante da série Jujutsu Kaisen . Ele e seus dois irmãos,KechizueEsoestão encarnados emCursed Womb: Death PaintingsNo. Choso inicialmente ficou do ladoda aliança deKenjakuMahitoaté descobrir que uma dasusuário da maldiçãoera o cruel pai dos Ventres da Pintura da Morte,Noritoshi Kamo. Ele também descobriu queYuji Itadorié outro parente de Kenjaku e se alinhou com seu novo “irmão mais novo”."
     },
     {
         "id":"2",
         "photo": "https://culturardotblog.files.wordpress.com/2023/11/20231011-jujutsu-kaisen-e36-nanami.webp",
-        "title": "olha o Nanami ai ",
-        "description": "Nanami é um personagem legal"
+        "title": "Nanami",
+        "description": "Kento Nanami (七なな海み建けんvocêと Nanami Kento ? )é um personagem coadjuvante importante em Jujutsu Kaisen . Ele foi um ex-aluno daTokyo Jujutsu High, onde foi aluno do primeiro ano deSatoru GojoeSuguru Geto. Nanami inicialmente deixou o Jujutsu High depois de se formar para se tornar um assalariado, mas retornou quatro anos depois para continuar trabalhando comofeiticeiro de jujutsu. Enquanto trabalhava como feiticeira do Jujutsu High, Nanami foi classificada como nota 1 e operava principalmente fora do campus de Tóquio. Com a introdução de Satoru, ele também se tornou um mentor próximo de Yuji Itadori ."
     },
     {
         "id":"3",
         "photo": "https://miro.medium.com/v2/resize:fit:1400/0*ipPc_EAVWsagz7Kt",
-        "title": "olha o Toji ai ",
-        "description": "Toji é um personagem legal"
+        "title": "Toji",
+        "description": "Toji Fushiguro (伏ふし黒ぐろ甚とう爾じ Fushiguro Toji ? ), nascidoToji Zenin(“ぜん院いん甚とう爾じ Zen'in Toji ? )é um personagem recorrente na série Jujutsu Kaisen . Ele era um ex-membro do clã Zenin e o infame assassino conhecido comoSorcerer Killer(術じゅつ師し殺ごろしJutsushi Goroshi ? ) , contratado pela Time Vessel Association entre outros grupos durante seu tempo como usuário não-maldição . Ele também era pai de Megumi Fushiguro e ex-inimigo de Satoru Gojo . Enquanto trabalhava para o Star Religious Group, Toji atua como o principal antagonista do Arco Passado de Gojo ."
     },
     {
         "id":"4",
         "photo": "https://i.pinimg.com/originals/21/a7/03/21a703f0833f2ba382691a1fb7a95ff7.jpg",
-        "title": "olha o Itadori ai ",
-        "description": "Itadori é um personagem legal"
+        "title": "Itadori",
+        "description": "Yuji Itadori (虎いた杖どり悠ゆう仁じ Itadori Yūji?) is the main protagonist of the Jujutsu Kaisen series. He is the son of Jin Itadori and Kaori Itadori, and the grandson of Wasuke Itadori. Yuji was living a normal life in Sendai City until he encountered Megumi and ate one of Sukuna's fingers. After becoming Sukuna's vessel, Yuji began attending Tokyo Jujutsu High alongside Megumi and Nobara as first-year students."
     }
 ]
   
